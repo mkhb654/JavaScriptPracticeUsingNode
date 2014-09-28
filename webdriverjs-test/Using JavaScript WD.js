125f75f07809a9f26230bee7822ea621e6faca9a
Muntaz <mkhb654@gmail.com>
2014-09-27 22:11:19 -0400

    adding nodemon globally and using nodemon-inspector

--- webdriverjs-test/Using JavaScript WD.js
+++ webdriverjs-test/Using JavaScript WD.js
@@ -1,8 +1,27 @@
-/**
- * Created by mkaleem on 9/27/14.
- */
-var assert = require ('assert');
-  test= reqire('selenium-webdriver/testing'),
-      webdriver =require('selenium-webdriver');
+///**
+// * Created by mkaleem on 9/27/14.
+// */
+//var assert = require ('assert');
+//var selenium-server-standalone-2.42.2jar = require ('server-standalone');
+//  test= require('selenium-server);
+//      webdriver =require('selenium-webdriver');
+//
+//
+//
+//test.describe('google search', function(){
+//    test.it('should work', function )
+//}
+var webdriver = require('selenium-webdriver'),
+    SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
 
-test.describe()
+var server = new SeleniumServer(pathToSeleniumJar, {
+    port: 4444
+});
+
+server.start();
+
+var driver = new webdriver.Builder().
+    usingServer(server.address()).
+    withCapabilities(webdriver.Capabilities.firefox()).
+    build();
+// ...
---
+++ webdriverjs-test/testingUsingJavascriptTest.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mkaleem on 9/27/14.
+ */
+
+var http = require('http');
+
+http.createServer (function(request, response) {
+     Respnse.writeHead
+}
