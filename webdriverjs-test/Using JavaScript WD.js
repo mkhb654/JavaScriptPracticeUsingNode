cb470bf27165a99a05dd2cc9b09f25b40a336534
Muntaz <mkhb654@gmail.com>
2014-09-27 21:24:41 -0400

    https://code.google.com/p/selenium/wiki/WebDriverJs

--- webdriverjs-test/Using JavaScript WD.js
+++ webdriverjs-test/Using JavaScript WD.js
@@ -1,5 +1,8 @@
 /**
  * Created by mkaleem on 9/27/14.
  */
+var assert = require ('assert');
+  test= reqire('selenium-webdriver/testing'),
+      webdriver =require('selenium-webdriver');
 
-
+test.describe()
